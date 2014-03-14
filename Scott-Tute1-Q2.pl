3d8d71e672ba9733aaf1d3370092b84e66dccc52
Scott-Boyle <sjboyle@gmail.com>
2014-03-14 16:31:06 +1100

    Updated response
    Just thought this was better form

--- Scott-Tute1-Q2.pl
+++ Scott-Tute1-Q2.pl
@@ -9,7 +9,7 @@ use strict;
 my @days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
 my $weekDay;
 my $firstMonday = int(rand(7) + 1);
-print "$firstMonday\n";
+#print "$firstMonday\n";
 print "What date would you like to know the day for? ";
 my $userDay = <STDIN>;
 chomp ($userDay);
@@ -20,15 +20,14 @@ warn "Date may be outside the bound of the current month!\n" if ($userDay > 28 &
 if ($userDay <= $firstMonday)
 {
   $weekDay = $days[0 - $userDay];
-  print "Your day is $weekDay.\n";
 }
   elsif (($userDay - $firstMonday) < 8)
   {
 	$weekDay = $days[($userDay - $firstMonday) - 1];
-	print "Your day is $weekDay.\n";
   }
 	else
 	{
 	  $weekDay = $days[($userDay - $firstMonday) % 7];
-	  print "Your day is $weekDay.\n";
 	}
+
+print "Your day is $weekDay.\n";
